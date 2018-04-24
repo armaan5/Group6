cb3a707c337320bb877d23e0f724a0f7848c2279
Sajen Shrestha <Sajen.shrestha@mavs.uta.edu>
2018-04-24 13:24:05 -0500

    Added to Repo

---
+++ Testapp/app/src/main/java/com/example/ssaze/testapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.ssaze.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+    EditText ed1,ed2;
+    TextView tx1; int counter = 3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.sign_in_button);
+        ed1 = (EditText)findViewById(R.id.editText3);
+        ed2 = (EditText)findViewById(R.id.editText4);
+
+        b2 = (Button)findViewById(R.id.cancel_button);
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                            tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
